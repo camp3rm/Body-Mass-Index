3b864885f5932c3670963bd0845caff24fa7a58c
Olek <96504837+camp3rm@users.noreply.github.com>
2024-10-07 20:47:45 +0200

    Changed the code for better functionality
    Changed the code in several places for better functionality, as well as the code to validate the input field

--- js/index.js
+++ js/index.js
@@ -3,71 +3,141 @@ const description = document.querySelector('.BMI-description');
 const bmiCalculator = document.querySelector('.bmi-calculator');
 const langChange = document.querySelector('#langChange')
 const ranges = document.querySelector('.value-ranges');
-const poepleWeight = document.querySelector('.weight');
-const poepleHeight = document.querySelector('.height');
-const calculateBMI = document.querySelector('.calculate');
+const calculateBtn = document.querySelector('.calculate');
 const resultField = document.querySelector('.result-field');
-const result = document.querySelector('.result')
-
+const result = document.querySelector('.result');
+const reloadPage = document.querySelector(".repeat-calculate");
+const regExp = /^\d{1,3}(\.\d{1,2})?$/;
 
 openCalculate.addEventListener('click', () => {
 	description.classList.add('hidden');
 	bmiCalculator.classList.remove('hidden');
+	document.addEventListener('DOMContentLoaded', function () {
+		window.scrollTo(0, 0);
+	});
 })
 
-// calculating body mass index
-function calculate() {
-	let weight = parseFloat(poepleWeight.value);
-	let height = parseFloat(poepleHeight.value);
-
-	if (weight > 0 && height > 0) {
-		let bmi = Math.round(weight / (Math.pow(height, 2)));
-		resultField.innerHTML = `Twój BMI: ${bmi}`;
-
-	} else {
-		resultField.innerHTML = `Proszę wprowadzić poprawne wartości!`;
-	}
-	poepleWeight.value = '';
-	poepleHeight.value = '';
+async function calculate() {
+	const savedLang = localStorage.getItem('language');
+	const translations = await loadTranslations();
+	const weight = parseFloat(peopleWeight.value)
+	const height = parseFloat(peopleHeight.value)
+	let bmi = Math.round(weight / (Math.pow(height, 2)));
+	peopleHeight.value = "";
+	peopleWeight.value = "";
+	let resultText = translations[savedLang].result;
+	resultField.textContent = `${resultText}` + ` ${bmi}`;
 	ranges.classList.remove('hidden');
 	result.classList.remove('hidden');
+	reloadPage.classList.remove('hidden');
+	peopleHeight.disabled = true;
+	peopleWeight.disabled = true;
 }
-calculateBMI.addEventListener('click', calculate);
 
+function validation(e) {
+	const value = e.target.value;
+	if (!value || !regExp.test(value)) {
+		if (e.target.id == "weight") {
+			document.getElementById("errorMessageWeight").classList.remove("valid");
+			document.getElementById("errorMessageWeight").classList.add("invalid");
+			calculateBtn.disabled = true;
+			result.classList.remove("invalid");
+			ranges.classList.remove("invalid");
+			result.classList.add("valid");
+			ranges.classList.add("valid")
+			return
+		}
+		if (e.target.id == "height") {
+			document.getElementById("errorMessageHeight").classList.remove("valid");
+			document.getElementById("errorMessageHeight").classList.add("invalid");
+			calculateBtn.disabled = true;
+			result.classList.remove("invalid");
+			ranges.classList.remove("invalid");
+			result.classList.add("valid");
+			ranges.classList.add("valid")
+			return
+		}
 
-const translationUrl = '../assets/lang.json';
+	} else {
+		if (e.target.id == "weight") {
+			document.getElementById("errorMessageWeight").classList.remove("invalid");
+			document.getElementById("errorMessageWeight").classList.add("valid");
+			calculateBtn.disabled = false;
+			result.classList.remove('valid');
+			ranges.classList.remove('valid');
+			result.classList.add("invalid");
+			ranges.classList.add("invalid")
+			return
+		}
+		if (e.target.id == "height") {
+			console.log(e.target.length);
+			document.getElementById("errorMessageHeight").classList.remove("invalid");
+			document.getElementById("errorMessageHeight").classList.add("valid");
+			calculateBtn.disabled = false;
+			result.classList.remove('valid');
+			ranges.classList.remove('valid');
+			result.classList.add("invalid");
+			ranges.classList.add("invalid")
+			return
+		}
+
+	}
+}
+
+const peopleWeight = document.querySelector('.weight');
+const peopleHeight = document.querySelector('.height');
+calculateBtn.addEventListener('click', calculate);
+peopleWeight.addEventListener('input', validation);
+peopleHeight.addEventListener('input', validation);
+
+const translationUrl = "./assets/lang.json";
+
 async function loadTranslations() {
 	try {
 		const response = await fetch(translationUrl);
 		if (!response.ok) {
 			throw new Error('Failed to load translations');
 		}
-		const data = await response.json();
-		return data;
+		return await response.json();
 	} catch (error) {
 		console.error('Error fetching translations:', error);
 	}
-};
+}
 
 async function changeLang(lang) {
 	const translations = await loadTranslations();
 	if (translations && translations[lang]) {
-		document.getElementById('bmiHeader').textContent = translations[lang].bmiHeader;
+		document.querySelectorAll('[data-translation]').forEach(elem => {
+			const dataTranslationValue = elem.getAttribute('data-translation');
+			const translation = translations[lang][dataTranslationValue];
+			if (translation) {
+				if (elem.tagName.toLowerCase() === 'input') {
+					elem.setAttribute('placeholder', translation);
+				} else {
+					elem.textContent = translation;
+				}
+			}
+		});
 	} else {
 		console.error('Translations for selected language not found.');
-	}
+	};
 };
 
-langChange.addEventListener('change', (e) => {
-	const selectedLang = e.target.value;
-	changeLang(selectedLang);
-});
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
-	const defaultLanguage = 'pl';
-	langChange.value = defaultLanguage;
-	changeLang(defaultLanguage); // Змінюємо мову при завантаженні сторінки
+	const savedLang = localStorage.getItem('language') || "pl";
+	changeLang(savedLang);
+	if (langChange) {
+		langChange.value = savedLang;
+	}
 });
 
+langChange.addEventListener('change', (e) => {
+	const selectLang = e.target.value;
+	changeLang(selectLang);
+	localStorage.setItem('language', selectLang);
+});
 
 
